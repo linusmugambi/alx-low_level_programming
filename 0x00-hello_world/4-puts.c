27942ef26e5fa668cecf2d228c25a2cc6729a487
linusmugambi <linusmugambi8@gmail>
2022-03-14 19:45:48 -0700

    my commit message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,22 @@
-#include <studio.h>
+
+#include <studio.h>                                           
+/**                                                           
+ *main - entry point                                          
+ *                                                            
+ *Return: 0 after printing the function                       
+ */                                                           
+int main(void)                                                
+{                                                             
+puts("\"programming is like building a multilingual puzzle"); 
+return (0);                                                   
+}#include <studio.h>
 /**
-*main - entry point
-*
-*Return:0 after printing the function
-*/
+ *main - entry point
+ *
+ *Return: 0 after printing the function
+ */
 int main(void)
 {
 puts("\"programming is like building a multilingual puzzle");
 return (0);
+}
