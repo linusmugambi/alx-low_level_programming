cc9e14614c660927f127def780144fb555fd9f3f
linusmugambi <linusmugambi8@gmail.com>
2022-03-30 22:30:13 +0300

    function that prints a string, followed by new line

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,43 +1,68 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
- * _strlen - determines length of string
- * @s: the string to check
+ * move_past_star - iterates past asterisk
+ * @s2: the second string, can contain wildcard
  *
- * Return: the integer length
+ * Return: the pointer past star
  */
-int _strlen(char *s)
+char *move_past_star(char *s2)
 {
-	return (*s ? 1 + _strlen(s + 1) : 0);
+	if (*s2 == '*')
+		return (move_past_star(s2 + 1));
+	else
+		return (s2);
 }
 
 /**
- * do_is_palindrome - recursive palindrome check
- * @s: the string to check
- * @l: the string length
- * @i: the string iterator
+ * inception - makes magic a reality
+ * @s1: the first string
+ * @s2: the second string, can contain wildcard
  *
- * Return: 1 if true, 0 if false
+ * Return: 1 if identical, 0 if false
  */
-int do_is_palindrome(char *s, int l, int i)
+int inception(char *s1, char *s2)
 {
-	if (i >= l / 2)
+	int ret = 0;
+
+	if (*s1 == 0)
+		return (0);
+	if (*s1 == *s2)
+		ret += wildcmp(s1 + 1, s2 + 1);
+	ret += inception(s1 + 1, s2);
+	return (ret);
+}
+
+/**
+ * wildcmp - compares two strings lexicographically
+ * @s1: the first string
+ * @s2: the second string, can contain wildcard
+ *
+ * Return: 1 if identical, 0 if false
+ */
+int wildcmp(char *s1, char *s2)
+{
+	int ret = 0;
+
+	if (!*s1 && *s2 == '*' && !*move_past_star(s2))
 		return (1);
-	if (*(s + i) == *(s + l - i - 1))
-		return (do_is_palindrome(s, l, i + 1));
+	if (*s1 == *s2)
+	{
+		if (!*s1)
+			return (1);
+		return (wildcmp(s1 + 1, *s2 == '*' ? s2 : s2 + 1));
+	}
+	if (!*s1 || !s2)
+		return (0);
+	if (*s2 == '*')
+	{
+		s2 = move_past_star(s2);
+		if (!*s2)
+			return (1);
+		if (*s1 == *s2)
+			ret += wildcmp(s1 + 1, s2 + 1);
+		ret += inception(s1, s2);
+		return (!!ret);
+	}
 	return (0);
-}
-
-/**
- * is_palindrome - determines if a string is a palindrome
- * @s: the string to check
- *
- * Return: 1 if true, 0 if false
- */
-int is_palindrome(char *s)
-{
-	int l = _strlen(s);
-
-	return (do_is_palindrome(s, l, 0));
 }
