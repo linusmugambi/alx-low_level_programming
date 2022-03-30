0cc2cbf3d9d57cf172af14eb7902d65dc4171ea0
linusmugambi <linusmugambi8@gmail.com>
2022-03-30 04:42:18 +0300

    the header file

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+int_putchar(char);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int_strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void point_chessboard(char (a*) [8]);
+void point_diagsums(int *, int size);
+void set_string(char **s, char *to);
